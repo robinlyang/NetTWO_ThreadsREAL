4a1dd44dae80ca461d0ca05e177f1864907a82db
robinlyang <robiny@gmail.com>
2016-11-28 18:44:28 -0700

    Initial Project

--- NetTWO_ThreadsREAL/Form1.cs
+++ NetTWO_ThreadsREAL/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace NetTWO_ThreadsREAL
 {
@@ -15,6 +16,33 @@ namespace NetTWO_ThreadsREAL
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            stuff s = new stuff();
+            //s.hi(); -> this creates an infinite loop
+            Thread t = new Thread(new ThreadStart(s.hi)); //declares the thread, but hasn't started it yet
+            t.Start();  //this starts the t thread (i.e. s.hi)
+
+
+        }
+    }
+    public class stuff
+    {
+        public void hi()
+        {
+            while(true)
+            {
+                MessageBox.Show("Hi");
+                Thread.Sleep(5000);
+            }
+            
         }
     }
 }
