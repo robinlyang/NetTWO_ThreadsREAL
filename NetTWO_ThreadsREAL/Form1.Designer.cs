98a461de93883e5966c7e3087a080f8e8d901d72
robinlyang <robiny@gmail.com>
2016-11-28 20:22:43 -0700

    More threads

--- NetTWO_ThreadsREAL/Form1.Designer.cs
+++ NetTWO_ThreadsREAL/Form1.Designer.cs
@@ -28,9 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // button1
@@ -60,11 +63,27 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(342, 52);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 71);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "hi";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(583, 386);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
@@ -81,6 +100,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- NetTWO_ThreadsREAL/Form1.cs
+++ NetTWO_ThreadsREAL/Form1.cs
@@ -18,7 +18,9 @@ namespace NetTWO_ThreadsREAL
         public static int total = 0;
         //this will be used for LOCKING the threads to prevent one starting before another is complete
         //it can be any type (e.g., int), any number, any name, but it can't be null
-        public static object obj = 1; 
+        public static object obj = 1;
+        // from notes: "-	-	Use Static Variable"
+        public static string data = "";
         public Form1()
         {
             InitializeComponent();
@@ -51,6 +53,24 @@ namespace NetTWO_ThreadsREAL
             Thread.Sleep(5000);
             MessageBox.Show(total.ToString());
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            stuff s = new stuff();
+            Thread t = new Thread(new ThreadStart(s.greet));
+            t.Name = "Hi from Program";  //this could be any variable
+            t.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(data != "")
+            {
+                textBox1.Text = data;
+                data = "";
+            }
+            
+        }
     }
     public class stuff
     {
@@ -74,6 +94,12 @@ namespace NetTWO_ThreadsREAL
                     Form1.total = Form1.total + 1;
                 }
             }
+        }
+        public void greet()  // from notes: "-	Use thread name"
+        {
+            Form1.data = "Stuff from thread";
+            Thread id = Thread.CurrentThread;
+            MessageBox.Show(id.Name);
         }
     }
 }
